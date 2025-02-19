1afaf64ca04a6f26e079ee84e500b5ad708319dd
uji52 <uji.52724@gmail.com>
2025-02-19 23:12:23 +0900

    Bootstrap start

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # uji52.com
+
+
+
+
+### Bootstrap関連メモ
+
+[Bootstrap and Vite](https://getbootstrap.jp/docs/5.3/getting-started/vite)
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import '../scss/styles.scss'
+
+// import * as bootstrap from 'bootstrap' // サイズを気にせずBootstrap全体をimportする場合
+import { Tooltip, Toast, Popover } from 'bootstrap';
