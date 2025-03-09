72bc99c6685e8c2de50abcf7af0d70f68388c7ae
uji52 <uji.52724@gmail.com>
2025-03-09 12:01:11 +0900

    favicon一括配置

--- vite.config.js
+++ vite.config.js
@@ -31,8 +31,25 @@ export default {
         icons: [
           {
             src: '/favicon.ico',
-            sizes: '256x256',
+            sizes: '48x48',
             type: 'image/x-icon'
+          },
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
           }
         ]
       }
