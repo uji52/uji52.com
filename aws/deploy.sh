b517ec79119e9b7dca0db4a9733767dcb619a72d
uji52 <uji.52724@gmail.com>
2025-03-23 12:16:21 +0900

    ファイル名調整

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,1 +1,2 @@
-aws cloudformation deploy --template-file aws/templates/api-gateway-ses.yml --stack-name uji52com-dev-apigw-ses --parameter-overrides EmailSNSTopicArn=arn:aws:sns:ap-northeast-1:xxxxxxxx:SendEmail --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file aws/templates/api-gateway.yml --stack-name uji52com-dev-apigw --parameter-overrides EmailSNSTopicArn=arn:aws:sns:ap-northeast-1:xxxxxxxxxxxx:SendEmail Environment=dev --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file aws/templates/api-gateway.yml --stack-name uji52com-prd-apigw --parameter-overrides EmailSNSTopicArn=arn:aws:sns:ap-northeast-1:xxxxxxxxxxxx:SendEmail Environment=prd --capabilities CAPABILITY_IAM
